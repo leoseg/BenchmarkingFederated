568b3c975c4408b1f28879834ee4d9501c42c882
leoseg <leoseeger16@googlemail.com>
2023-01-04 19:40:40 +0100

    updated slurm scripts

--- bash_scripts/train.sh
+++ bash_scripts/train.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 #SBATCH --nodes=1
-#SBATCH --job-name=process_data
+#SBATCH --job-name=training_unsupervised
+#SBATCH --partition=clara-job
 #SBATCH --time=1-00:00:00
 #SBATCH --mail-type=begin        # send email when job begins
 #SBATCH --mail-type=end          # send email when job ends
 #SBATCH --mail-type=fail         # send email if job fails
 #SBATCH --mail-user=leoseeger16@gmail.com
+module load CUDAcore/11.2.1
+module load cuDNN/8.1.0.77-CUDA-11.2.1
 module load Anaconda3
 source activate venv
 pip3 install -r requirements.txt
-python .././process_data_script.py
+python ../CentralizedApproach/train.py
