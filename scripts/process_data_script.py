f54f0497d63f31a156e3b3988ca28630fcb2a459
leoseg <leoseeger16@googlemail.com>
2023-01-11 13:49:22 +0100

    added process data scripts

---
+++ scripts/process_data_script.py
@@ -0,0 +1,11 @@
+from utils.rawdataprocessing_utils import genexpr_txt_file_to_csv,genexpr_anotation_txt_to_csv
+from pyreadr import read_r
+import pandas as pd
+data = read_r("DataGenExpression/Datasets.RData")
+dataframes = []
+for i in range(1,4):
+    dataframes.append(data[f"data.{i}"].T.join(data[f"info.{i}"]))
+    dataframes[i-1].to_csv(f"DataGenExpression/Dataset{i}.csv")
+all_data = pd.concat(dataframes)
+all_data.to_csv("DataGenExpression/Alldata.csv")
+
