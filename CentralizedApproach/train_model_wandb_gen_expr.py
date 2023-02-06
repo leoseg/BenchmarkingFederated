55ddf815702979db82e8ee827d23abd668f034e8
leoseg <leoseeger16@googlemail.com>
2023-02-06 09:38:18 +0100

    edited train script for configuring centrail model

--- CentralizedApproach/train_model_wandb_gen_expr.py
+++ CentralizedApproach/train_model_wandb_gen_expr.py
@@ -1,39 +1,48 @@
-import datetime
-
-from utils.data_utils import load_gen_data, create_X_y, load_gen_data_as_train_test_split
+from utils.data_utils import load_gen_data, create_X_y
 from utils.models import get_seq_nn_model
-from keras.metrics import Precision,Recall,AUC
 from sklearn.model_selection import StratifiedKFold
 import wandb
 import tensorflow as tf
 from wandb.keras import WandbCallback
 from utils.config import configs
+import argparse
 
-configs = dict(
-    batch_size = 512,
-    epochs = 100,
-    optimizer = 'adam',
-    loss = "binary_crossentropy",
-    metrics = ["accuracy",AUC(curve="PR"),Precision(),Recall()],
-    earlystopping_patience = 5,
-    num_nodes = 1024,
-    dropout_rate = 0.3,
-    l1_v = 0.0,
-    l2_v = 0.005,
-    n_splits = 5
+parser = argparse.ArgumentParser(
+        prog="benchmark_central_model_metrics.py",
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+    )
 
+parser.add_argument(
+    "--num_nodes", type=int, help="num of nodes for each layer",default=configs["num_nodes"]
 )
+
+parser.add_argument(
+    "--dropout_rate", type=float, help="dropout rate",default=configs["dropout_rate"]
+)
+parser.add_argument(
+    "--l1_v",type=float,help="l1 kernel regularizer",default=configs["l1_v"]
+)
+parser.add_argument(
+    "--data_path", type=str, help="path of data to load",default=configs["data_path"]
+)
+# print help if no argument is specified
+args = parser.parse_args()
+
 #create train test data
-data_path = "../../DataGenExpression/Dataset1.csv"
+data_path = args.data_path
 data_name = data_path.split("/")[2].split(".")[0]
 modelname = data_path.split("/")[-1].split(".")[0]
 df = load_gen_data(data_path)
 X, Y= create_X_y(df)
 kfold = StratifiedKFold(n_splits=configs["n_splits"],shuffle=True,random_state=0)
 
+num_nodes = args.num_nodes
+dropout_rate = args.dropout_rate
+l1_v = args.l1_v
+
 
 for count,(train,test) in enumerate(kfold.split(X,Y)):
-    wandb.init(project=f"benchmark-central_{data_name}_with_tf_dataset", config=configs, job_type='train',name=f"k_fold_{count}")
+    wandb.init(project=f"choose-best-config-central_{data_name}_gen_expr", config=configs, job_type='train',name=f"k_fold_{count}")
 
     client_dataset = tf.data.Dataset.from_tensor_slices((X.iloc[train], Y[train])).shuffle(configs["shuffle"])
     # Define WandbCallback for experiment tracking
@@ -49,13 +58,13 @@ for count,(train,test) in enumerate(kfold.split(X,Y)):
 
     train_ds = client_dataset.take(train_ds_size).shuffle(10000,reshuffle_each_iteration=True).batch(configs["batch_size"]).repeat(configs["epochs"])
     valid_ds= client_dataset.skip(train_ds_size)
-    model = get_seq_nn_model(X.iloc[train].shape[1], configs["num_nodes"],configs["dropout_rate"], configs["l1_v"], configs["l2_v"])
+    model = get_seq_nn_model(X.iloc[train].shape[1], num_nodes,dropout_rate ,l1_v, configs["l2_v"])
     model.compile(optimizer=configs["optimizer"],
                   loss=configs["loss"],
                   metrics=configs["metrics"])
 
-    model.fit(train_ds,validation_freq=10,validation_data=(valid_ds),callbacks=[wandb_callback])
-    #model.fit(X.iloc[train], Y[train], epochs=configs["epochs"], batch_size=configs["batch_size"], validation_freq = 10, validation_split=0.2,callbacks=[wandb_callback])
+    #model.fit(train_ds,validation_freq=10,validation_data=(valid_ds),callbacks=[wandb_callback])
+    model.fit(X.iloc[train], Y[train], epochs=configs["epochs"], batch_size=configs["batch_size"], validation_freq = 10, validation_split=0.2,callbacks=[wandb_callback])
 
     #evaluate utils
     score = model.evaluate(X.iloc[test], Y[test], verbose = 0,return_dict=True)
--- cluster_bash_scripts/benchmark_central_gen_expr_variations_model.sh
+++ cluster_bash_scripts/benchmark_central_gen_expr_variations_model.sh
@@ -9,12 +9,14 @@
 #SBATCH --mail-user=leoseeger16@gmail.com
 #module load Python/3.10.4-GCCcore-11.3.0
 #python3 -m venv venv
-#export PYTHONPATH="${PYTHONPATH}:../."
+export PYTHONPATH="${PYTHONPATH}:../."
 #source../venv/bin/activate
 #pip3 install --upgrade pip
 #pip3 install -r ../requirements.txt
 WANDB_API_KEY=$WANDB_API_KEY
-python ../CentralizedApproach/benchmark_central.py --num_nodes 512 --dropout_rate 0.3 --l1_v 0.0 --data_path "../DataGenExpression/Alldata.csv"
-python ../CentralizedApproach/benchmark_central.py --num_nodes 1024 --dropout_rate 0.3 --l1_v 0.0 --data_path "../DataGenExpression/Alldata.csv"
-python ../CentralizedApproach/benchmark_central.py --num_nodes 256 --dropout_rate 0.5 --l1_v 0.005 --data_path "../DataGenExpression/Alldata.csv"
-
+cd ..
+# shellcheck disable=SC2164
+cd CentralizedApproach
+python train_model_wandb_gen_expr.py --num_nodes 512 --dropout_rate 0.3 --l1_v 0.0 --data_path "../DataGenExpression/Alldata.csv"
+python train_model_wandb_gen_expr.py --num_nodes 1024 --dropout_rate 0.3 --l1_v 0.0 --data_path "../DataGenExpression/Alldata.csv"
+python train_model_wandb_gen_expr.py --num_nodes 256 --dropout_rate 0.5 --l1_v 0.005 --data_path "../DataGenExpression/Alldata.csv"
