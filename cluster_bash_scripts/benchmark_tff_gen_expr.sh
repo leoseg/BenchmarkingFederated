8344cb02f404bdb3da9cbacbae69422deedccdda
leoseg <leoseeger16@googlemail.com>
2023-02-06 21:12:22 +0100

    added more cpus and mem to slurm jobs

--- cluster_bash_scripts/benchmark_tff_gen_expr.sh
+++ cluster_bash_scripts/benchmark_tff_gen_expr.sh
@@ -3,6 +3,8 @@
 #SBATCH --job-name=training_unsupervised
 #SBATCH --partition=clara
 #SBATCH --time=1-00:00:00
+#SBATCH --ntasks=11
+#SBATCH --mem=50G
 #SBATCH --mail-type=begin        # send email when job begins
 #SBATCH --mail-type=end          # send email when job ends
 #SBATCH --mail-type=fail         # send email if job fails
