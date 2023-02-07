3fc478b14e11e966fa444a91ee89edf6a58c18a8
leoseg <leoseeger16@googlemail.com>
2023-02-07 17:44:48 +0100

    updated benchmark central script

--- cluster_bash_scripts/benchmark_central_system.sh
+++ cluster_bash_scripts/benchmark_central_system.sh
@@ -23,7 +23,7 @@ DATA_NAME=$(basename "$DATA_PATH" .csv)
 cd CentralizedApproach || exit
 for (( repeat = 0; repeat < $REPEATS; repeat++ ))
 do
-  python benchmark_central.py --run_repeat $repeat --data_path $DATA_PATH &
+  python benchmark_central__system_metrics.py --run_repeat $repeat --data_path $DATA_PATH &
   psrecord $! --log "CentralizedApproach/timelogs/central_model_repeat_${repeat}.txt" --intervall 0.5
   project_name="benchmark-central_${DATA_NAME}_system_metrics"
   python ../scripts/mem_data_to_wandb.py --logs_path "CentralizedApproach/timelogs/central_model_repeat_${repeat}.txt" --project_name $project_name --run_name $run_name --group_name "central"  --memory_type "central"
