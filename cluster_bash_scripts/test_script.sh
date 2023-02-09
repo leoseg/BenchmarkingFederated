e91ff5eeaf2d18c7fcfe493a3b99af50a95f4609
leoseg <leoseeger16@googlemail.com>
2023-02-09 17:23:18 +0100

    modified benchmark script for tff

--- cluster_bash_scripts/test_script.sh
+++ cluster_bash_scripts/test_script.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sbatch --partition=clara --job-name=test --time=2-00:00:00 --n-tasks=1 --mem=20g --cpus-per-task=11
+sbatch --partition=clara --job-name=test --time=2-00:00:00 --ntasks=1 --mem=20g --cpus-per-task=11
