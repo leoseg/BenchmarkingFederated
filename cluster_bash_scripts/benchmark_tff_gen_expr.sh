06e39dd55ba1b7946ecb5439a1f6a7cdb5f0951d
leoseg <leoseeger16@googlemail.com>
2023-02-09 15:13:23 +0100

    modified benchmark script for tff

--- cluster_bash_scripts/benchmark_tff_gen_expr.sh
+++ cluster_bash_scripts/benchmark_tff_gen_expr.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
-#SBATCH --job-name=tff_benchmark
-#SBATCH --partition=clara
-#SBATCH --time=2-00:00:00
-#SBATCH --cpus-per-task=11
-#SBATCH --mem-per-cpu=4g
-#SBATCH --ntasks=12
-#SBATCH --mail-type=begin        # send email when job begins
-#SBATCH --mail-type=end          # send email when job ends
-#SBATCH --mail-type=fail         # send email if job fails
-#SBATCH --mail-user=leoseeger16@gmail.com
 module load Python/3.10.4-GCCcore-11.3.0
 export PYTHONPATH="${PYTHONPATH}:../."
 WANDB_API_KEY=$1
@@ -28,7 +18,7 @@ do
   for client_num in {3,5,10}
   do
     echo "Starten srun with ${client_num} clients and ${rounds} rounds"
-    bash tff_balanced_benchmark.sh "../DataGenExpression/Alldata.csv" $client_num $rounds $WANDB_API_KEY $NUM_REPEATS &
+    sbatch tff_balanced_benchmark.sh "../DataGenExpression/Alldata.csv" $client_num $rounds $WANDB_API_KEY $NUM_REPEATS &
   done
 done
 wait
