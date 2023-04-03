5f920b08dd90fe88c236783f7e88e4202a5e344e
leoseg <leoseeger16@googlemail.com>
2023-04-03 10:40:14 +0200

    changed config

--- utils/config.py
+++ utils/config.py
@@ -7,7 +7,7 @@ from keras.optimizers import Adam,SGD
 from keras.losses import BinaryCrossentropy,SparseCategoricalCrossentropy
 tff_time_logging_directory = "timelogs/tff_logs_time.txt"
 flw_time_logging_directory = "timelogs/flw_logs_time.txt"
-version = "without_global_seed_dropout_seed_set_partition_bug_fixed"
+version = "seeds_everywhere"
 DATA_PATH = ""
 if os.environ["USECASE"] == "test":
     configs = dict(
