94e7f720fbafec588a5fcbf4f2a4d73213aab549
leoseg <leoseeger16@googlemail.com>
2023-04-29 11:09:51 +0200

    added plotting of model metrics

---
+++ utils/db_utils.py
@@ -0,0 +1,115 @@
+import yaml
+from pymongo import MongoClient
+SECRETS_FILE = "../../utils/secrets.yaml"
+
+class MongoDBHandler:
+    """A class to handle MongoDB operations."""
+
+    DB_NAME = "masterthesis"
+    COLLECTION_NAME = "benchmarks"
+
+    def __init__(self):
+        """
+        Initialize the MongoDBHandler class.
+        """
+        self.client = MongoClient(self.load_mongodb_url())
+        self.db = self.client[self.DB_NAME]
+        self.collection = self.db[self.COLLECTION_NAME]
+
+    def update_benchmark_data(self, data, name):
+        """
+        Update or insert a document with the specified data and name.
+
+        :param data: The dictionary to be added to the document.
+        :type data: dict
+        :param name: The name of the document.
+        :type name: str
+        """
+        existing_document = self.collection.find_one({"name": name})
+
+        if existing_document:
+            self.collection.update_one({"name": name}, {"$set": {"data": data}})
+        else:
+            self.collection.insert_one({"data": data, "name": name})
+
+    @staticmethod
+    def load_mongodb_url():
+        """
+        Load the MongoDB URL from the specified YAML file.
+
+        :return: The MongoDB URL.
+        :rtype: str
+        """
+        with open(SECRETS_FILE, "r") as file:
+            secrets = yaml.safe_load(file)
+            return secrets["mongodb_adress"]
+
+    def get_data_by_name(self, name):
+        """
+        Get data from a document by its name.
+
+        :param name: The name of the document.
+        :type name: str
+        :return: The data from the document or None if the document doesn't exist.
+        :rtype: dict or None
+        """
+        document = self.collection.find_one({"name": name})
+        return document["data"] if document else None
+
+#
+# class PostgresHandler:
+#     """A class to handle PostgreSQL operations."""
+#
+#     DB_NAME = "masterthesis"
+#     TABLE_NAME = "benchmarks"
+#
+#     def __init__(self):
+#         """
+#         Initialize the PostgresHandler class.
+#         """
+#         self.conn = self.connect_to_postgres()
+#
+#     def connect_to_postgres(self):
+#         """
+#         Connect to the PostgreSQL database.
+#
+#         :return: A connection object.
+#         :rtype: psycopg2.extensions.connection
+#         """
+#         with open(SECRETS_FILE, "r") as file:
+#             secrets = yaml.safe_load(file)
+#             db_config = secrets["postgres"]
+#
+#         return psycopg2.connect(
+#             dbname=db_config["dbname"],
+#             user=db_config["user"],
+#             password=db_config["password"],
+#             host=db_config["host"],
+#             port=db_config["port"],
+#         )
+#
+#     def load_dataframe(self, df, name):
+#         """
+#         Load a pandas DataFrame into a PostgreSQL table.
+#
+#         :param df: The DataFrame to be loaded.
+#         :param name: The name of the data to later load it with this name.
+#         """
+#         df["name"] = name
+#         df.to_sql("benchmarks", con=self.conn, if_exists='append')
+#
+#     def get_data_by_name(self, name):
+#         """
+#         Get data from a PostgreSQL table row by its name.
+#         :param name: The name of the row.
+#         :return: The data from the row or None if the row doesn't exist.
+#         """
+#         query = sql.SQL(f"SELECT * FROM benchmark WHERE name = %s")
+#         with self.conn.cursor() as cursor:
+#             cursor.execute(query, (name,))
+#             row = cursor.fetchone()
+#             if row:
+#                 column_names = [desc[0] for desc in cursor.description]
+#                 return dict(zip(column_names, row))
+#             else:
+#                 return None
--- utils/plotting.py
+++ utils/plotting.py
@@ -4,16 +4,20 @@ import seaborn as sns
 from config import configs
 import matplotlib.pyplot as plt
 ENTITY = "Scads"
+if configs.get("usecase") == 2:
+    ROUNDS = [1,2,4,8]
+else:
+    ROUNDS = [1,2,5,10]
 
 
-
-def get_group_stats(project:str,groups:list,version:str,metric_names:list):
+def get_group_stats(project:str,groups:list,version:str,metric_names:list,mode:str):
     """
     Get the mean of the metrics for each group of a project
     :param project: name of the project
     :param groups: names of the groups
     :param version: version of the runs
     :param metric_names: metrics to get the mean of
+    :param mode: mode of the project can be "unweighted" or "system" or "balanced"
     :return: dictionary with the mean of the metrics for each group
     """
     api = wandb.Api()
@@ -23,20 +27,14 @@ def get_group_stats(project:str,groups:list,version:str,metric_names:list):
         metrics_dicts = []
         for run in runs:
             if run.config.get("version") == version:
-                # If the run summary does not contain one of the metrics, get it from the history
-                if any([metric_name not in run.summary.keys() for metric_name in metric_names]):
-                    history = run.history()
-                    metrics_dicts.append({key:history.get(key).iloc[-1] for key in metric_names})
-                # If the run summary contains the metric, get it from the summary
-                else:
-                    metrics_dicts.append({key:run.summary.get(key) for key in metric_names})
-        metrics  = {key: [metrics_dict.get(key) for metrics_dict in metrics_dicts if metrics_dict.get(key) is not None ] for key in metric_names}
+                metrics_dicts.append(get_metrics_from_run(run, metric_names,group,mode))
+        metrics  = put_metrics_together(metrics_dicts)
         project_metrics[group] = metrics
     return project_metrics
 
 
 
-def get_stats_for_usecase(groups,version = None,mode="balanced",as_df=False):
+def get_stats_for_usecase(groups,version = None,mode="balanced",rounds=None):
     """
     Get the mean of the metrics for each group of all projecets for that usecase
     :param mode: mode of the project can be "unweighted" or "system" or "balanced"
@@ -48,12 +46,14 @@ def get_stats_for_usecase(groups,version = None,mode="balanced",as_df=False):
     project_prefix = "usecase"
     metrics_prefix = "model"
     data_path = configs.get("data_path").split("/")[-1].split(".")[0]
-    rounds  = [1,2,5,10]
+    if rounds is None:
+        rounds  = ROUNDS
     round_metrics = []
     if not version:
         version = configs.get("version")
     if mode == "system":
         metrics_prefix ="system"
+        version = None
         metrics_names = ["memory_client","memory_server","round_time","client_time"]
     else:
         metrics_names = [element.name for element in configs.get("metrics")]
@@ -61,51 +61,106 @@ def get_stats_for_usecase(groups,version = None,mode="balanced",as_df=False):
     if mode == "unweighted":
         project_prefix = "unweightedusecase"
         metrics_names.extend([name +"_global" for name in metrics_names])
-    if usecase == 2:
-        rounds = [1,2,4,8]
     for round in rounds:
         project = f"{project_prefix}_{str(usecase)}_benchmark_rounds_{round}_{data_path}_{metrics_prefix}_metrics"
-        round_metrics.append(get_group_stats(project,groups,version=version,metric_names=metrics_names))
+        round_metrics.append(get_group_stats(project,groups,version=version,metric_names=metrics_names,mode=mode))
     return round_metrics
 
 
-def transform_to_df(metrics_for_number_of_rounds:dict,metric_name:str,central_data=None):
+def transform_scenario_metrics_to_df(metrics:dict,metric_name:str,round_num):
     """
-    Transform the metrics for one roundconfiguration to a pandas dataframe for plotting
-    :param round_metrics:
+    Transform the metrics for all roundconfiguration to a pandas dataframe for plotting
+    :param metrics: metrics for all roundconfiguration
+    :param metric_name: name of metric
+    :round_num: number of rounds used for multiplying time metrics
     :return: df with the metrics for one roundconfiguration
     """
-    rows = []
-    for groupname,metrics in metrics_for_number_of_rounds.items():
+    dfs= []
+    for groupname,metrics in metrics.items():
         for i in range(len(metrics[metric_name])):
-            row = {
-                "framework":groupname.split("_")[0],
-                "group":groupname.split("_")[1],
-                metric_name:metrics[metric_name][i]
-            }
-            rows.append(row)
-    if central_data:
-        for i in range(len(central_data[metric_name])):
-            row = {
-                "framework":"central",
-                "group":"central",
-                metric_name:central_data[metric_name][i]
-            }
-            rows.append(row)
-    df = pd.DataFrame(data = rows,columns=["framework", "group", metric_name])
+            dfs.append(transform_to_df(metrics,metric_name,groupname.split("_")[0],groupname.split("_")[1],round_num, round_num))
+    df = pd.concat(dfs)
     return df
 
-def plot_swarmplots(rounds_metrics,metric_name:str,central_data=None):
+
+def transform_to_df(metrics:dict,metric_name,framework,group,round_configuration,round_num=1):
     """
-    Plots swarmplots for the metrics of all roundconfigurations
-    :param df:
+    Transform the metrics for one roundconfiguration and one metric to a pandas dataframe for plotting
+    :param metrics: metrics for one roundconfiguration
+    :param metric_name: name of metric
+    :param framework: framework of the run
+    :param group: group of the run
+    :param round_configuration: roundconfiguration of the run
+    :param round_num: number of rounds used for multiplying time metrics
+    :return: df with the metrics for one roundconfiguration and one metric
+    """
+    rows = []
+    for i in range(len(metrics[metric_name])):
+        row = {
+            "framework": framework,
+            "group": group,
+            "round configuration": round_configuration
+        }
+        metric_value = metrics[metric_name][i] if "time" not in metric_name else metrics[metric_name][i] * round_num
+        row["metric"] = metric_value
+        rows.append(row)
+    df = pd.DataFrame(data=rows, columns=["framework", "group", "metric", "round configuration"])
+    return df
+
+
+
+def create_dfs_for_fl_metric(rounds_metrics,metric_name:str):
+    """
+    Creates a dataframe for each roundconfiguration for one metric and appends them together
+    :param rounds_metrics: metrics for each roundconfiguration
+    :param metric_name: name of metric
+    :return: df with all values for one metric
+    """
+    dfs = []
+    for index, metric_for_number_of_rounds in enumerate(rounds_metrics):
+        round_num = ROUNDS[index]
+        df = transform_scenario_metrics_to_df(metric_for_number_of_rounds, metric_name, round_num)
+        dfs.append(df)
+    df = pd.concat(dfs)
+    return df
+
+
+def seaborn_plot (x,y,hue,data,palette,title,dodge=True):
+    """
+    Plots a seaborn boxplot to easy exchange plottype
+    :param x: x axis data
+    :param y: y axis data
+    :param hue: hue data
+    :param data: data to plot
+    :param palette: color palette
+    :param title: title of the plot
+    :param dodge: dodge parameter
     :return:
     """
-    for metric_for_number_of_rounds in rounds_metrics:
-        df = transform_to_df(metric_for_number_of_rounds,metric_name,central_data)
-        sns.boxplot(x="group", y=metric_name, hue="framework",
-                    data=df, palette="Set2", dodge=True)
+    sns.boxplot(x=x, y=y, hue=hue,
+                data=data, palette=palette, dodge=dodge).set_title(title)
+    plt.show()
+
+def plot_swarmplots(df,metric_name:str):
+    """
+    Plots swarmplots for the given df which contains all data for onem metric
+    :param df: df with all data for one metric
+    :metric_name: name of metric to plot
+    """
+    for index,round_num in enumerate(ROUNDS):
+        round_df = df[df["round configuration"] == "central"]
+        round_df = pd.concat([round_df,df[df["round configuration"] == round_num]],ignore_index=True)
+        seaborn_plot("group", "metric", "framework", round_df, "Set2", f"Round configuration {round_num}")
         plt.show()
+    seaborn_plot("group", "metric", "framework", df, "Set2", f"Round configuration sumarized")
+    seaborn_plot("round configuration", "metric", "framework", df, "Set2", f"Group sumarized")
+    for group in df["group"].unique():
+        if group == "central":
+            continue
+        group_df = df[df["group"] == "central"]
+        group_df = pd.concat([group_df,df[df["group"] == group]],ignore_index=True)
+        seaborn_plot("round configuration", "metric", "framework", group_df, "Set2", f"Group {group}")
+
 
 
 def get_central_metrics(mode:str,metric_names:list):
@@ -119,20 +174,111 @@ def get_central_metrics(mode:str,metric_names:list):
     group = f"usecase_{usecase}"
     if mode == "system":
         project = "benchmark-central_system_metrics"
+        metric_names = ["training_time","memory_central"]
+        version = None
     else:
         project = "central_model_metrics"
+        version = "essential_seeds_42"
     api = wandb.Api()
     runs = api.runs(f"{ENTITY}/{project}",filters={"group": group})
     metrics_dicts = []
     for run in runs:
-        if run.name != "no_crossfold" and run.config.get("version") == "essential_seeds_42":
-            # If the run summary does not contain one of the metrics, get it from the history
-            if any([metric_name not in run.summary.keys() for metric_name in metric_names]):
+        if run.name != "no_crossfold" and run.config.get("version") == version:
+            metrics_dicts.append(get_metrics_from_run(run,metric_names,group,mode))
+    metrics = put_metrics_together(metrics_dicts)
+    return metrics
+
+
+def get_system_metrics(history, metric_names, group):
+    """
+    Get the system metrics from the history of a run
+    :param history: history of a run
+    :param metric_names: metric names to get
+    :param group: group of the run
+    :return: dictionary with the metrics
+    """
+    metrics = {}
+    for metric in metric_names:
+        if metric in ["memory_client","memory_server","memory_central"]:
+            # if the metric is a memory metric, get the sum, mean and max of the metric
+            memory_metrics = get_memory_metrics(group, history, metric)
+            metrics.update(memory_metrics)
+        elif metric in ["round_time","client_time","training_time"]:
+            # if the metric is a time metric, get the mean of the metric
+            time = history.get(metric)
+            if time is not None:
+                metrics[metric] = time.mean()
+                if metric == "client_time":
+                    # if the metric is client_time, get the first_round_time
+                    metrics["first_round_time"] = time.iloc[0]
+    if "client_time" in metrics.keys() and "round_time" in metrics.keys():
+        # if both client_time and round_time are in the metric_names, get the time_diff
+        metrics["time_diff"] = metrics["round_time"] - metrics["client_time"]
+    if "total_memory_client" in metrics.keys() and "total_memory_server" in metrics.keys():
+        # if both memory_client and memory_server are in metric_names calculate the sum
+        # of both metrics summed up
+        metrics["total_memory_fl"] = metrics["total_memory_client"] + metrics["total_memory_server"]
+    return metrics
+
+def get_memory_metrics(group, history, metric):
+    """
+    Get the sum, mean and max of a memory metric
+    :param group: group of the run
+    :param history: history of the run
+    :param metric: name of the metric
+    :return: dictionary with the sum, mean and max of the metric
+    """
+    memory_metrics = {}
+    memory = history.get(metric)
+    if memory is not None:
+        if metric == "memory_client":
+            number_of_clients = int(group.split("_")[-1])
+        else:
+            number_of_clients = 1
+        memory_metrics[f"total_{metric}"] = memory.sum() * number_of_clients
+        memory_metrics[f"mean_{metric}"] = memory.mean() * number_of_clients
+        memory_metrics[f"max_{metric}"] = memory.max() * number_of_clients
+    return memory_metrics
+def get_metrics_from_run(run:wandb.run,metric_names:list,group,mode:str):
+    """
+    Get the metrics from a run
+    :param run: run to get the metrics from
+    :param metric_names: names of the metrics to get
+    :param group: group of the run
+    :param group: mode of the run
+    :param mode: mode of the run
+    :return: dictionary with the metrics for the run
+    """
+    history = None
+    metrics = {}
+    if mode == "system":
+        history = run.history()
+        return get_system_metrics(history,metric_names,group)
+    for metric in metric_names:
+        if metric not in run.summary.keys():
+            # If the run summary does not contain the metric, get it from the history
+            if history is None:
                 history = run.history()
-                metrics_dicts.append({key: history.get(key).iloc[-1] for key in metric_names})
+            if history.get(metric) is not None:
+                metrics[metric] = history.get(metric).iloc[-1]
+        else:
             # If the run summary contains the metric, get it from the summary
-            else:
-                metrics_dicts.append({key: run.summary.get(key) for key in metric_names})
+            metrics[metric] = run.summary.get(metric)
+    return metrics
+
+
+
+
+
+def put_metrics_together(metrics_dicts:list):
+    """
+    Put the metrics of all runs into one dictionary
+    :param metrics_dicts: list of dictionaries with the metrics for each run
+    :return: dict with the averaged metrics for all runs
+    """
+    unique_keys = set()
+    for metrics_dict in metrics_dicts:
+        unique_keys.update(metrics_dict.keys())
     metrics = {key: [metrics_dict.get(key) for metrics_dict in metrics_dicts if metrics_dict.get(key) is not None]
-               for key in metric_names}
+               for key in unique_keys}
     return metrics
