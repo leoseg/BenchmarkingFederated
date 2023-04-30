c8a0ff8e93c686e77330a3e80a5b59219e45603b
leoseg <leoseeger16@googlemail.com>
2023-04-30 12:48:28 +0200

    added framework switch to extreme case script

--- cluster_bash_scripts/benchmark_many_clients.sh
+++ cluster_bash_scripts/benchmark_many_clients.sh
@@ -13,41 +13,45 @@ USECASE=$3
 export USECASE=$USECASE
 DATA_PATH=$4
 SYSTEM_ONLY=$5
+FRAMEWORK=$6
 cd ..
-python3.10 -m venv venvtff
-source venvtff/bin/activate
-python3 -c 'import sys; print(sys.version_info[:])'
-pip3 install --upgrade pip
-pip install -e utils
-pip3 install -r requirements.txt
-cd TensorflowFederated || exit
-# Choose rounds configuration depending on usecase
-if [ $3 =  "1" ] ||  [ $3 = "3" ] || [ $3 = "4" ]; then
-   round_config=(1 2 5 10)
-elif [  $3 = "2" ]; then
-   round_config=(1 2 4 8)
+if [ $FRAMEWORK = "TFF" ];then
+    python3.10 -m venv venvtff
+    source venvtff/bin/activate
+    python3 -c 'import sys; print(sys.version_info[:])'
+    pip3 install --upgrade pip
+    pip install -e utils
+    pip3 install -r requirements.txt
+    cd TensorflowFederated || exit
+    # Choose rounds configuration depending on usecase
+    if [ $3 =  "1" ] ||  [ $3 = "3" ] || [ $3 = "4" ]; then
+       round_config=(1 2 5 10)
+    elif [  $3 = "2" ]; then
+       round_config=(1 2 4 8)
+    fi
+    # Loops trough round and number of clients configuration
+    for rounds in "${round_config[@]}";
+    do
+      for client_num in {30,100}
+      do
+        bash tff_balanced_benchmark.sh $DATA_PATH $client_num $rounds $WANDB_API_KEY $NUM_REPEATS $SYSTEM_ONLY
+      done
+    done
 fi
-# Loops trough round and number of clients configuration
-for rounds in "${round_config[@]}";
-do
-  for client_num in {30,100}
+if [ $FRAMEWORK = "FLWR" ];then
+  cd Flower || exit
+  # Choose rounds configuration depending on usecase
+  if [ $3 =  "1" ] ||  [ $3 = "3" ] || [ $3 = "4" ]; then
+     round_config=(1 2 5 10)
+  elif [  $3 = "2" ]; then
+     round_config=(1 2 4 8)
+  fi
+  # Loops trough round and number of clients configuration
+  for rounds in "${round_config[@]}";
   do
-    bash tff_balanced_benchmark.sh $DATA_PATH $client_num $rounds $WANDB_API_KEY $NUM_REPEATS $SYSTEM_ONLY
+    for client_num in {30,100}
+    do
+      bash flwr_balanced_benchmark.sh $DATA_PATH $client_num $rounds $WANDB_API_KEY $NUM_REPEATS $SYSTEM_ONLY
+    done
   done
-done
-cd ..
-cd Flower || exit
-# Choose rounds configuration depending on usecase
-if [ $3 =  "1" ] ||  [ $3 = "3" ] || [ $3 = "4" ]; then
-   round_config=(1 2 5 10)
-elif [  $3 = "2" ]; then
-   round_config=(1 2 4 8)
 fi
-# Loops trough round and number of clients configuration
-for rounds in "${round_config[@]}";
-do
-  for client_num in {30,100}
-  do
-    bash flwr_balanced_benchmark.sh $DATA_PATH $client_num $rounds $WANDB_API_KEY $NUM_REPEATS $SYSTEM_ONLY
-  done
-done
